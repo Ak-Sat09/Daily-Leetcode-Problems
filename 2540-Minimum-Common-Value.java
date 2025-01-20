c76c6f73818dc6341ca465fbf7c70a37fe610f0b
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-01-20 16:06:44 +0530

    [2540] [Time Beats: 86.02%] [Memory Beats: 47.23%] - LeetPush

---
+++ 2540-Minimum-Common-Value.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int getCommon(int[] arr1, int[] arr2) {
+
+         int i =0;
+        int j = 0;
+  
+        while (i<arr1.length && j<arr2.length) {
+            if (arr1[i] == arr2[j]) {
+                 return arr1[i];
+            }
+            else if(arr1[i] < arr2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        return -1;
+    }
+}
