6436ca1b318e3343a0184e83e7274b93305eb2f3
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-02-01 11:11:30 +0530

    [3151] [Time Beats: 100.00%] [Memory Beats: 72.64%] - LeetPush

--- DCP-02-25/3151-Special-Array-I.java
+++ DCP-02-25/3151-Special-Array-I.java
@@ -1,11 +1,8 @@
 class Solution {
     public boolean isArraySpecial(int[] arr) {
         if(arr.length == 1)return true;
-        for(int j=1; j<arr.length; j++){
-            if((arr[j]%2) == arr[j-1] % 2){
-                return false;
-            }
-
+        for(int i=1; i<arr.length; i++){
+            if((arr[i] % 2) == arr[i-1] % 2)return false;
         }
         return true;
     }
