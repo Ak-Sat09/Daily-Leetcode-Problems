05d789e1105072af0800e741d7d70934ee073e31
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-02-10 08:41:24 +0530

    [3174] [Time Beats: 38.21%] [Memory Beats: 45.64%] - LeetPush

---
+++ DCP-02-25/3174-Clear-Digits.java
@@ -0,0 +1,14 @@
+ class Solution {
+    public String clearDigits(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(Character.isDigit(ch) && !stack.isEmpty())stack.pop();
+            else stack.push(ch);
+        }
+        StringBuilder str = new StringBuilder();
+        while(!stack.isEmpty()){
+            str.append(stack.pop());
+        }
+        return str.reverse().toString();
+    }
+}
