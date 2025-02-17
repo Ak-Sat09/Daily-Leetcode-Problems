54dee2ac68d03912c51f23fb38a051e56ea628d6
Anmol Mehla <166195625+Ak-Sat09@users.noreply.github.com>
2025-02-17 18:31:41 +0530

    [3] [Time Beats: 70.62%] [Memory Beats: 66.64%] - LeetPush

--- 3-Longest-Substring-Without-Repeating-Characters.java
+++ 3-Longest-Substring-Without-Repeating-Characters.java
@@ -4,15 +4,12 @@ class Solution {
         int left = 0; // Start of the sliding window
         Set<Character> seen = new HashSet<>();
 
-        for (int right = 0; right < s.length(); right++) {
-            // If character at 'right' is already in the set, shrink the window from the left
+        for (int right = 0; right < s.length(); right++) { 
             while (seen.contains(s.charAt(right))) {
                 seen.remove(s.charAt(left));
                 left++;
-            }
-            // Add the current character to the set
-            seen.add(s.charAt(right));
-            // Update the maximum length of the window
+            } 
+            seen.add(s.charAt(right)); 
             maxLength = Math.max(maxLength, right - left + 1);
         }
 
